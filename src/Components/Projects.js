3d1c4a3b487fcc09531a7ea99391a527a922e066
jackcornblum <jcornblum@gmail.com>
2021-09-07 14:52:48 -0400

    css

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -16,15 +16,15 @@ function Projects({projectRef, blogRef}) {
         
         <Row ref={projectRef} id="project-session">
             <Col id="project-left">
-                <div >
+                <div id="project-header">
                     <h2 className="header">Projects</h2>
                 </div>
 
                 <div id="div_top_hypers2">
                     <ul id="ul_top_hypers2">
-                        <li><a href="" class="a_top_hypers">   About   </a></li>
-                        <li onClick={handleProject} className="before"><a href="#project-section" class="a_top_hypers">   Projects   </a></li>
-                        <li onClick={handleBlog} className="before"><a href="#blog-section" class="a_top_hypers">   Blogs   </a></li>
+                        <li><a href="" class="a_top_hypers2">   About   </a></li>
+                        <li onClick={handleProject} className="before"><a href="#project-section" class="a_top_hypers2">   Projects   </a></li>
+                        <li onClick={handleBlog} className="before"><a href="#blog-section" class="a_top_hypers2">   Blogs   </a></li>
                     </ul>
                 </div>
             </Col>
