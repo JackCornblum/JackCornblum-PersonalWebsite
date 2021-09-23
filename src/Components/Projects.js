981ce26df00aec24b38481b867389c0db7bf0d6b
jackcornblum <jcornblum@gmail.com>
2021-09-23 14:46:32 -0400

    scroll functionality

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -37,6 +37,11 @@ function Projects({projectRef, blogRef, aboutRef}) {
                     <h3 className="project-title"><a href="http://secret-bayou-38884.herokuapp.com/login" target="_blank" class="a_top_hypers">Tarkov Helper</a></h3>
                     <iframe className="project-thumbnail" src="https://www.loom.com/embed/31585020ad8a4864ae6b40472b4da683" frameborder="0" webkitallowfullscreen="webkitallowfullscreen" mozallowfullscreen allowfullscreen="allowfullscreen" ></iframe>
                     <p className="white-font">Escape from Tarkov is a first-person shooter video game that focuses on achieving a high level of realism when it comes to weapon mechanics and customization. One problem that many new players of the game have is not only finding certain weapon parts to buy for a gun, but also finding which parts offer the best statistical advantages for various guns. To solve this issue, I designed Tarkov Helper. The app can take any gun, assuming there are parts offered for that gun, and generate a list of parts for the user to purchase in game and lets the user know where to find the part.</p>
+
+
+                    <h3 className="project-title"><a href="http://secret-bayou-38884.herokuapp.com/login" target="_blank" class="a_top_hypers">Tarkov Helper</a></h3>
+                    <iframe className="project-thumbnail" src="https://www.loom.com/embed/31585020ad8a4864ae6b40472b4da683" frameborder="0" webkitallowfullscreen="webkitallowfullscreen" mozallowfullscreen allowfullscreen="allowfullscreen" ></iframe>
+                    <p className="white-font">Escape from Tarkov is a first-person shooter video game that focuses on achieving a high level of realism when it comes to weapon mechanics and customization. One problem that many new players of the game have is not only finding certain weapon parts to buy for a gun, but also finding which parts offer the best statistical advantages for various guns. To solve this issue, I designed Tarkov Helper. The app can take any gun, assuming there are parts offered for that gun, and generate a list of parts for the user to purchase in game and lets the user know where to find the part.</p>
                 </div>
             </Col>
         </Row>
