ab112d75c24b06cbf975d1d35b8a410837ceb61d
jackcornblum <jcornblum@gmail.com>
2021-09-29 15:59:59 -0400

    setting up blogs

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,6 +1,16 @@
 import { Col, Row } from 'react-bootstrap'
+import { useState, useEffect } from 'react'
+import Article from './Article'
 
 function About({projectRef, blogRef, aboutRef}) {
+
+    const [articles, setArticles] = useState('')
+
+    useEffect(() => {
+        fetch("https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@jcornblum")
+        .then(res => res.json())
+        .then(data => setArticles(data.items))
+    }, [])
 
     function handleProject() {
         projectRef.current.scrollIntoView()
@@ -13,6 +23,13 @@ function About({projectRef, blogRef, aboutRef}) {
     function handleAbout() {
         aboutRef.current.scrollIntoView()
     }
+
+    let renderArticles = articles.map(a => {
+        <Article title={a.title} description={a.description} content={a.content} image={a.thumbnail} link={a.link} key={a.title} />
+    })
+
+
+
 
 
     return (
@@ -32,7 +49,7 @@ function About({projectRef, blogRef, aboutRef}) {
            
         </Col>
         <Col id="blogs">
-            <h1>here they are</h1>
+        
         </Col>
     </Row>
     )
---
+++ src/Components/Article.js
@@ -0,0 +1,11 @@
+import {Card} from 'react-bootstrap'
+
+function Article() {
+
+
+    return (
+        <div></div>
+    )
+}
+
+export default Article;
