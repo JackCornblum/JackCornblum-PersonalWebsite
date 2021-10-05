fb1353284909aa19775891db6891fd9b73981ff9
jackcornblum <jcornblum@gmail.com>
2021-10-05 13:42:14 -0400

    blogs

--- src/Components/Article.js
+++ src/Components/Article.js
@@ -1,12 +1,16 @@
 import {Card} from 'react-bootstrap'
 
 function Article({title, description, image, content, link}) {
+    console.log(description)
 
 
     return (
         <Card className="article-card">
             <Card.Img variant="top" src={image} />
+            <Card.Title>{title}</Card.Title>
+            <Card.Body href={link} target="_blank"><a href={link} target="_blank">Read the full thing here!</a></Card.Body>
         </Card>
+      
     )
 }
 
